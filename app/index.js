3bddf1ffe98fd8c156c1789e2a2931866d221b18
Ghost_KNA <ern777gavilanes@gmail.com>
2023-11-11 02:16:28 -0500

    prueba de engine

--- app/index.js
+++ app/index.js
@@ -5,6 +5,10 @@ import {fileURLToPath} from 'url';
 import {methods as authentication} from "./controllers/authentication.controller.js"
 import {methods as authorization} from "./middlewares/authorization.js";
 
+import mysql from 'mysql';
+
+
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 //Server
@@ -22,8 +26,27 @@ app.use(cookieParser())
 app.get("/",authorization.soloPublico, (req,res)=> res.sendFile(__dirname + "/pages/login.html"));
 app.get("/register",authorization.soloPublico,(req,res)=> res.sendFile(__dirname + "/pages/register.html"));
 app.get("/admin",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/admin/admin.html"));
-app.get("/admin/feudaldonate",authorization.soloGhost,(req,res)=> res.sendFile(__dirname + "/pages/admin/admin.html"));
+app.get("/admin/feudaldonate",authorization.soloGhost,(req,res)=> res.sendFile(__dirname + "/pages/admin/feudal-donate.html"));
 app.post("/api/login",authentication.login);
 app.post("/api/register",authentication.register);
 
+
+export const connection = mysql.createConnection({
+    host: 'localhost', // Cambia esto al host de tu base de datos
+    user: 'root', // Cambia esto al nombre de usuario de tu base de datos
+    password: 'aion777kna777', // Cambia esto a la contraseña de tu base de datos
+    database: 'lif_1' // Cambia esto al nombre de tu base de datos
+  });
+  
+  connection.connect((error) => {
+    if (error) {
+      console.error('Error al conectar a la base de datos:', error);
+      return;
+    }
+    console.log('Conexión exitosa a la base de datos');
+  });
+
+  
+  
+  export default connection;
 
--- app/public/admin.js
+++ app/public/admin.js
@@ -1,7 +1,25 @@
+//import  connection  from "../index";
 
-document.getElementById("closed_session").addEventListener("click", function() {
-  document.cookie ='jwt=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT; SameSite=None;Secure=true;';
+// document.getElementById("on").addEventListener("click", function () {
+  
+//   // Puedes utilizar la conexión en este archivo
+//   console.log("connection")
+//   const query = 'SELECT * FROM nombre_de_la_tabla'; // Reemplaza 'nombre_de_la_tabla' con el nombre de tu tabla
+
+//   connection.query(query, (error, results) => {
+//     if (error) {
+//       console.error('Error al ejecutar la consulta:', error);
+//       return;
+//     }
+//     res.render('feudal-donate', { resultados: results }); // Renderiza el archivo HTML y pasa los resultados como datos
+//   });
+// });
+
+document.getElementById("closed_session").addEventListener("click", function () {
+  document.cookie = 'jwt=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT; SameSite=None;Secure=true;';
   document.location.href = "/";
 });
+
+
 
 
